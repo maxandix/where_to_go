4d7e828f3afff65c5d3704145de93cda4cbd3c2e
Бирюков Максим <maxandix@gmail.com>
2020-08-21 01:36:05 +0300

    Add frontend

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,13 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index_page(request):
-    return HttpResponse('''<!DOCTYPE html>
-<html>
-<head>
-  <title>Стартовая</title>
-</head>
-<body>
-  <h1>Здесь будет карта</h1>
-</body>
-</html>''')
+    context = {}
+    return render(request, 'index.html', context)
